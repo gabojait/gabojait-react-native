3adba286945f92639264132379d28c6c654273d9
Yoning Lee <0517lyi@naver.com>
2022-08-07 15:28:26 +0900

    Feat:"추가 - color, textStyle 정리"

---
+++ src/presentation/res/styles/textStyles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native'
+
+const textStyles = StyleSheet.create({
+  weight1: {
+    fontWeight: '900',
+    fontFamily: 'Pretendard-Bold',
+  },
+  weight2: {
+    fontWeight: '600',
+    fontFamily: 'Pretendard-SemiBold',
+  },
+  weight3: {
+    fontWeight: '300',
+    fontFamily: 'Pretendard-Medium',
+  },
+  weight4: {
+    fontWeight: '100',
+    fontFamily: 'Pretendard-Light',
+  },
+  size1: {
+    fontSize: 30,
+  },
+  size2: {
+    fontSize: 22,
+  },
+  size3: {
+    fontSize: 17,
+  },
+  size4: {
+    fontSize: 14,
+  },
+  size5: {
+    fontSize: 13,
+  },
+})
+
+export default textStyles
