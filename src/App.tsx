0037e9302877fa6e055b074dc6a01e9626d5954a
KimWash <ckm0728wash@gmail.com>
2022-08-10 00:43:02 +0900

    FEAT: Add fonts on iOS

--- src/App.tsx
+++ src/App.tsx
@@ -16,42 +16,44 @@ import {theme} from '@/theme'
 
 import LoginInput from '@/presentation/components/LoginInput'
 import ProfileCard from '@/presentation/components/ProfileCard'
-import {FilledButton, OutlinedButton} from "@/presentation/components/Button"
+import {FilledButton, OutlinedButton} from '@/presentation/components/Button'
 import styles from './styles'
 
 const App = () => {
   const backgroundStyle = {
     flex: 1,
-    margin: 20,
   }
 
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
         <SafeAreaView style={backgroundStyle}>
-          <Text h3>Full Width Buttons</Text>
-          <FilledButton title="시작하기" />
-          <FilledButton title="시작하기" disabled />
-          <Text h3>Wraped Buttons (with flex)</Text>
-          <View style={styles.wrapButtonConatiner}>
+          <ScrollView
+            style={{
+              padding: 20,
+            }}>
+            <Text h3>Full Width Buttons</Text>
             <FilledButton title="시작하기" />
             <FilledButton title="시작하기" disabled />
-          </View>
-          <Text h3>Outlined Buttons (Round)</Text>
-          <OutlinedButton title="삭제하기" disabled radius={theme.rd1} />
-          <OutlinedButton title="종료하기" radius={theme.rd1} />
-          <OutlinedButton title="해산하기" disabled radius={theme.rd1} />
-          <Text h3>Outlined Buttons</Text>
-          <View style={styles.wrapButtonConatiner}>
-            <OutlinedButton title="중복확인" radius={theme.rd3} highlighted />
-            <OutlinedButton title="확인완료" radius={theme.rd3} highlighted disabled />
-          </View>
-          <AirbnbRating
-            count={10}
-            reviews={['최악이에요', '별로에요', '괜찮아요', '좋아요', '최고에요']}
-          />
-          <FAB />
-          <ScrollView>
+            <Text h3>Wraped Buttons (with flex)</Text>
+            <View style={styles.wrapButtonConatiner}>
+              <FilledButton title="시작하기" />
+              <FilledButton title="시작하기" disabled />
+            </View>
+            <Text h3>Outlined Buttons (Round)</Text>
+            <OutlinedButton title="삭제하기" disabled radius={theme.rd1} />
+            <OutlinedButton title="종료하기" radius={theme.rd1} />
+            <OutlinedButton title="해산하기" disabled radius={theme.rd1} />
+            <Text h3>Outlined Buttons</Text>
+            <View style={styles.wrapButtonConatiner}>
+              <OutlinedButton title="중복확인" radius={theme.rd3} highlighted />
+              <OutlinedButton title="확인완료" radius={theme.rd3} highlighted disabled />
+            </View>
+            <AirbnbRating
+              count={5}
+              reviews={['최악이에요', '별로에요', '괜찮아요', '좋아요', '최고에요']}
+            />
+            <FAB />
             <LoginInput state={'default'} />
             <LoginInput state={'success'} />
             <LoginInput state={'error'} />
@@ -67,6 +69,5 @@ const App = () => {
     </ThemeProvider>
   )
 }
-
 
 export default App
