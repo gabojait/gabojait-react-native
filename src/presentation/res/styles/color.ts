ded5aa70a6a0504393f66b315026d3b24871018e
이용인 <yongin0517@gmail.com>
2022-12-26 17:46:31 +0900

    feat: 추가 - lightGrey color
    알파벳 순서 정렬도 했습니다.

--- src/presentation/res/styles/color.ts
+++ src/presentation/res/styles/color.ts
@@ -1,11 +1,13 @@
 export default {
-  primary: '#1cdf71',
-  disable: '#d9d9d9',
   barIcon: '#6c6c6c',
-  error: '#fc0101',
-  white: '#ffffff',
   black: '#000000',
+  disable: '#d9d9d9',
+  error: '#fc0101',
+  lightGrey:'#eeeeee',
+  primary: '#1cdf71',
   progress1: '#ffdb20',
   progress2: '#f06823',
   progress3: '#f04823',
+  transparent: '#ffffff00',
+  white: '#ffffff',
 }
