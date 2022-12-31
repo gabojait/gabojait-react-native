49686c2bd787d1ddd384744abac0bbe2955f4d23
이용인 <yongin0517@gmail.com>
2022-12-31 16:20:18 +0900

    feat:업데이트-MenuCard 컴포넌트
    ThemeProvider 중복을 막기 위해 ThemeProvider를 통한 스타일지정을 제거하고, 각 하위컴포넌트에 스타일을 추가함.

--- src/presentation/components/ MenuCard.tsx
+++ src/presentation/components/ MenuCard.tsx
@@ -1,5 +1,5 @@
 import React, {ButtonHTMLAttributes, useState} from 'react'
-import {Icon, Input, Card, Text, createTheme, ThemeProvider, useTheme} from '@rneui/themed'
+import {Icon, Input, Card, Text, createTheme, ThemeProvider, useTheme, CardProps, TextProps} from '@rneui/themed'
 import {StyleSheet, TextInput, View} from 'react-native'
 import textStyles from '../res/styles/textStyles'
 import colors from '../res/styles/color'
@@ -14,48 +14,28 @@ interface MenuCardProps {
 
 const MenuCard = ({title, iconName}: MenuCardProps) => {
   return (
-    <ThemeProvider
-      theme={theme_default}>
-      <Card>
-        <Icon
-            name = {iconName}
-            type='ionicon' // 프로젝트 전용 커스텀 아이콘이 미완성인 관계로 ionicon으로 임시지정
-        />
-        <Text>{title}</Text>
-      </Card>
-    </ThemeProvider>
-  )
-}
-
-const theme_default = createTheme({
-  components: {
-    Card: {
-      containerStyle: {
-        alignItems: 'center',
+    <Card containerStyle={{
+      alignItems: 'center',
         backgroundColor:colors.white,
         borderColor:colors.transparent,
         shadowColor:colors.transparent,
         flex:1,
         justifyContent: 'center',
-        marginHorizontal: 5,
+        marginHorizontal: 4,
         marginVertical: 8,
-      },
-    },
-    Text: {
-      style: {
+    }}>
+      <Icon
+          name = {iconName}
+          type='ionicon' // 프로젝트 전용 커스텀 아이콘이 미완성인 관계로 ionicon으로 임시지정
+          size={50}
+          style={{alignSelf:'center'}}
+      />
+      <Text style={{
         alignSelf:'center',
         fontWeight: textstyles.weight3.fontWeight,
         fontFamily: textstyles.weight3.fontFamily,
-        fontSize:textstyles.size4.fontSize,
-      },
-    },
-    Icon:{
-        size: 50,
-        style:{
-            alignSelf:'center',
-        }
-    }
-  },
-})
-
+        fontSize:textstyles.size4.fontSize,}}>{title}</Text>
+    </Card>
+  )
+}
 export default MenuCard
