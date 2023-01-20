e40cc5fb5e870e75f17a92255cee1b3c6ca886bf
이용인 <yongin0517@gmail.com>
2023-01-21 00:56:44 +0900

    Feat: Update icons(gabojait, heart_circled)

--- src/presentation/components/icon/Heart_circled.js
+++ src/presentation/components/icon/Heart_circled.js
@@ -1,3 +1,3 @@
 import { createIconSetFromFontello } from "react-native-vector-icons"
 import fontelloConfig from  '@/assets/config.json'
-export default createIconSetFromFontello(fontelloConfig,"HeartCircled")
+export default createIconSetFromFontello(fontelloConfig,"circled_heart")
