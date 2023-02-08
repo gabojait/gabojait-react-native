eb52ced64d72c9191d4b6e0f9f8ee0db45809b80
이용인 <yongin0517@gmail.com>
2023-02-08 20:13:47 +0900

    FEAT:Update Screens

--- src/presentation/screens/Main/Home/Group/Detail.tsx
+++ src/presentation/screens/Main/Home/Group/Detail.tsx
@@ -1,9 +1,86 @@
-import { Text } from '@rneui/themed'
+import { FilledButton } from '@/presentation/components/Button'
+import CardWrapper from '@/presentation/components/CardWrapper'
+import { PartIcon } from '@/presentation/components/GroupListCard'
+import { GroupStackParamList } from '@/presentation/navigation/types'
+import { StackScreenProps } from '@react-navigation/stack'
+import { makeStyles, Text, useTheme } from '@rneui/themed'
 import React from 'react'
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
-const Detail = () => <View>
-  <Text>This is Post!</Text>
-</View>
+export type GroupStackParamListProps = StackScreenProps<GroupStackParamList, 'Detail'>
 
+const Detail = ({navigation}:GroupStackParamListProps) => {
+  const {theme} = useTheme() 
+  const styles = useStyles()
+
+  return (
+    <ScrollView style={styles.scrollView}>
+      <CardWrapper style={[styles.card,{minHeight: 243}]}>
+        <View>
+          <Text style={styles.teamname}>같이 앱 만들 사람 구해요</Text>
+          <View style={styles.partIcon}>
+            <PartIcon partInitial={'D'} size={23}/>
+            <PartIcon partInitial={'P'} size={23}/>
+            <PartIcon partInitial={'E'} size={23}/>
+          </View>
+          <FilledButton title={'함께 하기'} onPress={() => navigation.navigate('PositionSelector')}/>
+        </View>
+      </CardWrapper>
+      <CardWrapper style={[styles.card,{minHeight: 200}]}>
+        <View>
+        <Text style={styles.title}>프로젝트 설명</Text>
+        <Text style={styles.text}>애플의 신작 스마트워치인 애플워치6와 애플워치SE가 29일 국내에 출시된다.
+            애플은 22일 애플 공식 매장과 공인 리셀러, 통신사를 통해 국내에 애플워치6와 애플워치SE를 오는 29일 정식 출시한다고 밝혔다.
+        </Text>
+        </View>
+      </CardWrapper>
+      <CardWrapper style={[styles.card,{minHeight: 200}]}>
+        <View>
+        <Text style={styles.title}>바라는 점</Text>
+        <Text style={styles.text}>
+            사전주문은 23일 오후 3시부터 시작된다. 애플 홈페이지와 애플스토어 앱 등에서 주문 가능하다. 
+            가격은 애플워치6는 53만9000원부터, 애플워치SE는 35만9000원부터 시작된다.
+            이번에 발표된 애플워치6는 기존 심전도체크 기능에 이어 혈중 산소포화도 기능을 탑재했다. 
+         </Text>
+        </View>
+      </CardWrapper>
+    </ScrollView>
+  )
+}
+
+const useStyles = makeStyles((theme)=> ({
+  scrollView:{
+    backgroundColor: theme.colors.white,
+    paddingHorizontal:20,
+    paddingVertical:18,
+    flex:1,
+  },
+  card:{
+    paddingHorizontal:13,
+    paddingVertical:17,
+    marginBottom:10,
+  },
+  teamname:{
+    fontSize:theme.fontSize.lg,
+    fontWeight:theme.fontWeight.bold,
+    paddingLeft:5
+  },
+  title: {
+    fontSize:20,
+    fontWeight:theme.fontWeight.semibold,
+    paddingBottom: 5,
+    color:theme.colors.black
+  },
+  text: {
+    fontSize:11,
+    fontWeight:theme.fontWeight.light,
+    color:theme.colors.black,
+    lineHeight:22
+  },
+  partIcon:{
+    paddingTop: 30,
+    paddingBottom: 25, 
+    flexDirection: 'row'
+  }
+}))
 export default Detail
--- src/presentation/screens/Main/Home/Group/PositionSelector.tsx
+++ src/presentation/screens/Main/Home/Group/PositionSelector.tsx
@@ -1,9 +1,66 @@
-import { Text } from '@rneui/themed'
+import { FilledButton } from '@/presentation/components/Button'
+import CardWrapper from '@/presentation/components/CardWrapper'
+import { PartIcon } from '@/presentation/components/GroupListCard'
+import { GroupStackParamList } from '@/presentation/navigation/types'
+import { StackScreenProps } from '@react-navigation/stack'
+import { makeStyles, Text, useTheme } from '@rneui/themed'
 import React from 'react'
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
-const PositionSelector = () => <View>
-  <Text>Cool Frontend</Text><Text>Cool Backend</Text><Text>Cool Designer</Text>
-</View>
+export type GroupStackParamListProps = StackScreenProps<GroupStackParamList, 'PositionSelector'>
+
+const PositionSelector = () => {
+  const {theme} = useTheme() 
+  const styles = useStyles()
+  
+  return (
+    <ScrollView style={styles.scrollView}>
+      <CardWrapper style={[styles.card]}>
+        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+          <View style={{alignItems:'center'}}>
+            <PartIcon partInitial={'2/3'} size={23}/>
+            <Text style={styles.text}>Ui, Ux 디자이너</Text> 
+          </View>
+          <FilledButton title="함께하기" size="sm"/>
+        </View>
+      </CardWrapper>
+      <CardWrapper style={[styles.card]}>
+        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+          <View style={{alignItems:'center'}}>
+            <PartIcon partInitial={'2/3'} size={23}/>
+            <Text style={styles.text}>Ui, Ux 디자이너</Text> 
+          </View>
+          <FilledButton title="모집완료" disabled={true} size="sm"/>
+        </View>
+      </CardWrapper>
+      <CardWrapper style={[styles.card]}>
+        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+          <View style={{alignItems:'center'}}>
+            <PartIcon partInitial={'2/3'} size={23}/>
+            <Text style={styles.text}>Ui, Ux 디자이너</Text> 
+          </View>
+          <FilledButton title="함께하기" size="sm"/>
+        </View>
+      </CardWrapper>
+    </ScrollView>
+  )
+}
+
+const useStyles = makeStyles((theme)=> ({
+  scrollView:{
+    backgroundColor: theme.colors.white,
+    paddingHorizontal:20,
+    paddingVertical:18
+  },
+  card:{
+    padding:30,
+    marginBottom:10,
+  },
+  text:{
+    fontSize: theme.fontSize.sm,
+    fontWeight: theme.fontWeight.semibold,
+    paddingTop: 10
+  }
+}))
 
 export default PositionSelector
