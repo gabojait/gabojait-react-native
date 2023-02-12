6ed1f100ce1431b718fb4c85b3a5213adaaf145d
이용인 <yongin0517@gmail.com>
2023-02-12 12:08:20 +0900

    FEAT: Update PositionSelector Screen

--- src/presentation/screens/Main/Home/Group/PositionSelector.tsx
+++ src/presentation/screens/Main/Home/Group/PositionSelector.tsx
@@ -1,6 +1,7 @@
 import { FilledButton } from '@/presentation/components/Button'
 import CardWrapper from '@/presentation/components/CardWrapper'
 import { PartIcon } from '@/presentation/components/GroupListCard'
+import PositionIcon from '@/presentation/components/PositionIcon'
 import { GroupStackParamList } from '@/presentation/navigation/types'
 import { StackScreenProps } from '@react-navigation/stack'
 import { makeStyles, Text, useTheme } from '@rneui/themed'
@@ -16,30 +17,30 @@ const PositionSelector = () => {
   return (
     <ScrollView style={styles.scrollView}>
       <CardWrapper style={[styles.card]}>
-        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+        <View style={styles.container}>
           <View style={{alignItems:'center'}}>
-            <PartIcon partInitial={'2/3'} size={23}/>
+            <PositionIcon currentApplicant={2} vancancyNumber={3} textView={<Text style={styles.posiionText}>2/3</Text>}/>
             <Text style={styles.text}>Ui, Ux 디자이너</Text> 
           </View>
           <FilledButton title="함께하기" size="sm"/>
         </View>
       </CardWrapper>
       <CardWrapper style={[styles.card]}>
-        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+        <View style={[styles.container, {paddingStart:30}]}>
           <View style={{alignItems:'center'}}>
-            <PartIcon partInitial={'2/3'} size={23}/>
-            <Text style={styles.text}>Ui, Ux 디자이너</Text> 
+            <PositionIcon currentApplicant={1} vancancyNumber={1} textView={<Text style={styles.posiionText}>1/1</Text>}/>
+            <Text style={styles.text}>기획자</Text> 
           </View>
           <FilledButton title="모집완료" disabled={true} size="sm"/>
         </View>
       </CardWrapper>
       <CardWrapper style={[styles.card]}>
-        <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', width:'100%'}}>
+        <View style={[styles.container, {paddingStart:30}]}>
           <View style={{alignItems:'center'}}>
-            <PartIcon partInitial={'2/3'} size={23}/>
-            <Text style={styles.text}>Ui, Ux 디자이너</Text> 
+            <PositionIcon currentApplicant={2} vancancyNumber={2} textView={<Text style={styles.posiionText}>2/2</Text>}/>
+            <Text style={styles.text}>백엔드</Text>  
           </View>
-          <FilledButton title="함께하기" size="sm"/>
+          <FilledButton title="모집완료" disabled={true} size="sm"/>
         </View>
       </CardWrapper>
     </ScrollView>
@@ -56,10 +57,21 @@ const useStyles = makeStyles((theme)=> ({
     padding:30,
     marginBottom:10,
   },
+  container:{
+    flexDirection:'row', 
+    justifyContent:'space-between', 
+    alignItems:'center', 
+    width:'100%', 
+    minHeight:100, 
+  },
   text:{
     fontSize: theme.fontSize.sm,
     fontWeight: theme.fontWeight.semibold,
-    paddingTop: 10
+    paddingTop: 10,
+  },
+  posiionText:{
+    fontSize: 20,
+    fontWeight: theme.fontWeight.bold
   }
 }))
 
