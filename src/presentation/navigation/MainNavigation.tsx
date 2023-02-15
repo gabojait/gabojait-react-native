e87999497b5a7d73e425660d72138ca29b209a19
이용인 <yongin0517@gmail.com>
2023-02-15 14:11:48 +0900

    DESIGN: Update MainNavigation style

--- src/presentation/navigation/MainNavigation.tsx
+++ src/presentation/navigation/MainNavigation.tsx
@@ -9,6 +9,7 @@ import Team from '../screens/Main/Team'
 import { RootStackNavigationProps } from './RootNavigation'
 import { MainStackParamList} from './types'
 import CustomIcon from '@/presentation/components/icon/Gabojait'
+import { useTheme } from '@rneui/themed'
 
 type MainNavigationProp<
 T extends keyof MainStackParamList = 'Home'
@@ -22,29 +23,37 @@ const MainBottomTab = createBottomTabNavigator()
 
 // 바텀네비게이션
 const MainNavigation = () => {
-
+  const {theme} = useTheme()
   return (
-    <MainBottomTab.Navigator backBehavior='none'>
+    <MainBottomTab.Navigator 
+      backBehavior='none' 
+      screenOptions={{
+        tabBarActiveTintColor:theme.colors.primary, 
+        tabBarInactiveTintColor:theme.colors.black, 
+        tabBarLabelStyle:{fontSize:13, fontWeight:theme.fontWeight.semibold, paddingTop:10},
+        tabBarStyle:{minHeight:60, paddingVertical:10}
+      }}
+    >
       <MainBottomTab.Group screenOptions={{headerShown: false}} >
         <MainBottomTab.Screen
           name="홈"
           component={Home}
           options={{
-            tabBarIcon: ({size, color}) => <CustomIcon name="home" size={size} color={color} />,
+            tabBarIcon: ({size, color}) => <CustomIcon name="home" size={size} color={color}/>,
           }}
         />
         <MainBottomTab.Screen
           name="팀페이지"
           component={Team}
           options={{
-            tabBarIcon: ({size, color}) => <Icon name="music" size={size} color={color} />,
+            tabBarIcon: ({size, color}) => <CustomIcon name="people" size={size} color={color}/>,
           }}
         />
         <MainBottomTab.Screen
           name="마이페이지"
           component={MyPage}
           options={{
-            tabBarIcon: ({size, color}) => <Icon name="user" size={size} color={color} />,
+            tabBarIcon: ({size, color}) => <CustomIcon name="person" size={size} color={color}/>,
           }}
         />
       </MainBottomTab.Group>
