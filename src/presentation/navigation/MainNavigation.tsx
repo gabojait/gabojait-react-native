acb03ed5437d4353137da4f769a688706cb9beec
이용인 <yongin0517@gmail.com>
2023-02-28 15:11:12 +0900

    FEAT: Modify BackHeader to TitleWithCloseHeader
    -기존 CustomHeader 수정으로 안돼서 새로 만듦

--- src/presentation/navigation/MainNavigation.tsx
+++ src/presentation/navigation/MainNavigation.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 import {MainStackParamList} from './types'
 import Profile from '../screens/Main/MyPage/Profile'
 import CloseHeader from '../screens/Headers/CloseHeader'
-import BackHeader from '../screens/Headers/BackHeader'
+import BackHeader from '../screens/Headers/TitleWithCloseHeader'
 import PositionSelector from '../screens/Main/Home/Group/PositionSelector'
 import TitleHeader from '../screens/Headers/TitleHeader'
 import GroupEditor from '../screens/Main/Home/Group/GroupEditor'
 import GroupDetail from '../screens/Main/Home/Group/GroupDetail'
+import ApplyStatus from '../screens/Main/MyPage/ApplyStatus'
+import TeamHistory from '../screens/Main/MyPage/TeamHistory'
+import TitleCenterHeader from '../components/TitleCenterHeader'
+import TitleWithCloseHeader from '../screens/Headers/TitleWithCloseHeader'
 
 const Main = createStackNavigator<MainStackParamList>()
 
@@ -16,12 +20,20 @@ const MainNavigation = () => {
   return (
     <Main.Navigator initialRouteName='GroupDetail'>
       <Main.Screen name="Profile" options={{header: CloseHeader}} component={Profile}></Main.Screen>
+      <Main.Screen name="ApplyStatus" component={ApplyStatus} options={{
+        header: TitleWithCloseHeader,
+        headerTitle: "지원현황"
+      }}/>
+      <Main.Screen name="TeamHistory" component={TeamHistory} options={{
+        header: TitleWithCloseHeader,
+        headerTitle: "팀 히스토리"
+      }}/>
       <Main.Screen name="GroupDetail" component={GroupDetail} options={{
         header: BackHeader,
         headerTitle: ""
       }}/>
       <Main.Screen name="PositionSelector" component={PositionSelector} options={{
-        header: BackHeader,
+        header: TitleWithCloseHeader,
         headerTitle: "포지션 선택"
       }}/>
       <Main.Screen name="GroupEditor" component={GroupEditor} options={{
