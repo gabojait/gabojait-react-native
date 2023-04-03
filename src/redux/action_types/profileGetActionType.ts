8be5b72016b57cbf70e375a3701b2b3f38d5c8f7
Lee YongIn <67788699+LeeYongIn0517@users.noreply.github.com>
2023-04-04 01:44:05 +0900

    REMOVE

--- src/redux/action_types/profileGetActionType.ts
+++
@@ -1,9 +0,0 @@
-import { ActionType } from "typesafe-actions"
-import * as actions from '@/redux/action/profileGetAction'
-import { AsyncState } from "@/lib/reducerUtils"
-import ProfileViewDto from "@/model/Profile/ProfileViewDto"
-
-export type ProfileGetAsyncActionType = ActionType<typeof actions>
-export type ProfileGetState = {
-    profileGetResult: AsyncState<ProfileViewDto, Error>
-}
