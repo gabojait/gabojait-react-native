9b443cd3c446d362e0240bc80dac0c32e03c4497
이용인 <yongin0517@gmail.com>
2023-07-02 12:59:50 +0900

    FEAT: Modify useCreateTeam

--- src/reactQuery/useCreateTeam.ts
+++ src/reactQuery/useCreateTeam.ts
@@ -2,16 +2,10 @@ import {createTeam} from '@/api/team'
 import TeamRequestDto from '@/model/Team/TeamRequestDto'
 import {useMutation} from 'react-query'
 
-interface useCreateTeamProps {
-  onSuccess: () => void
-}
-
-export const useCreateTeam = ({onSuccess}: useCreateTeamProps) => {
+//TODO: teamEditor이랑 같이 쓸 수 있는 거 만들기
+export const useCreateTeam = () => {
   return useMutation({
     mutationKey: ['createTeam'],
     mutationFn: (dto: TeamRequestDto) => createTeam(dto),
-    onSuccess: () => {
-      onSuccess()
-    },
   })
 }
