8a8ad6779bb5e4ff7a6367a57b479ae48fd8840c
KimWash <ckm0728wash@gmail.com>
2023-07-26 09:53:26 +0900

    CHORE: FCM 적용 및 fabric 비활성화

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { SafeAreaView } from 'react-native';
-import { ThemeProvider } from '@rneui/themed';
-import { RootNavigation } from './presentation/navigation/RootNavigation';
+import {SafeAreaView} from 'react-native';
+import {ThemeProvider} from '@rneui/themed';
+import {RootNavigation} from './presentation/navigation/RootNavigation';
 import allReducers from '@/redux/reducers';
 import ReduxThunk from 'redux-thunk';
-import { ModalProvider } from './presentation/components/modal/context';
-import { theme } from './presentation/theme';
+import {ModalProvider} from './presentation/components/modal/context';
+import {theme} from './presentation/theme';
 import ErrorBoundary from './presentation/components/errorComponent/ErrorBoundary';
-import { Fallback500, Fallback503 } from './presentation/components/errorComponent/GeneralFallback';
+import {Fallback500, Fallback503} from './presentation/components/errorComponent/GeneralFallback';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { createLogger } from 'redux-logger';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {createLogger} from 'redux-logger';
 import './assets/locales/index';
 
 const queryClient = new QueryClient();
@@ -19,41 +19,35 @@ const logger = createLogger();
 const store = createStore(allReducers, applyMiddleware(ReduxThunk, logger));
 
 import NetInfo from '@react-native-community/netinfo'
-import { onlineManager } from 'react-query'
+import {onlineManager} from 'react-query'
 
 onlineManager.setEventListener(setOnline => {
-  return NetInfo.addEventListener(state => {
-    setOnline(!!state.isConnected)
-  })
+    return NetInfo.addEventListener(state => {
+        setOnline(!!state.isConnected)
+    })
 })
 
-if (__DEV__) {
-  import('react-query-native-devtools').then(({ addPlugin }) => {
-    addPlugin({ queryClient });
-  });
-}
-
 const App = () => {
-  const backgroundStyle = {
-    flex: 1,
-  };
-  // const modalRef = useRef<CustomModalRef>()
+    const backgroundStyle = {
+        flex: 1,
+    };
+    // const modalRef = useRef<CustomModalRef>()
 
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ErrorBoundary fallback500={Fallback500()} fallback503={Fallback503()}>
-          <QueryClientProvider client={queryClient}>
-            <ModalProvider>
-              <SafeAreaView style={backgroundStyle}>
-                <RootNavigation />
-              </SafeAreaView>
-            </ModalProvider>
-          </QueryClientProvider>
-        </ErrorBoundary>
-      </ThemeProvider>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <ErrorBoundary fallback500={Fallback500()} fallback503={Fallback503()}>
+                    <QueryClientProvider client={queryClient}>
+                        <ModalProvider>
+                            <SafeAreaView style={backgroundStyle}>
+                                <RootNavigation/>
+                            </SafeAreaView>
+                        </ModalProvider>
+                    </QueryClientProvider>
+                </ErrorBoundary>
+            </ThemeProvider>
+        </Provider>
+    );
 };
 
 export default App;
--- src/presentation/screens/Onboarding/SplashScreen.tsx
+++ src/presentation/screens/Onboarding/SplashScreen.tsx
@@ -1,36 +1,83 @@
-import {RootStackScreenProps} from '@/presentation/navigation/types'
-import { getUser } from '@/redux/action/login'
-import {useAppDispatch, useAppSelector} from '@/redux/hooks'
-import React, {useEffect} from 'react'
-import {View} from 'react-native'
-import Splash from 'react-native-splash-screen'
+import {RootStackScreenProps} from '@/presentation/navigation/types';
+import {getUser} from '@/redux/action/login';
+import {useAppDispatch, useAppSelector} from '@/redux/hooks';
+import React, {useEffect} from 'react';
+import {Alert, PermissionsAndroid, Platform, View} from 'react-native';
+import Splash from 'react-native-splash-screen';
+import messaging from '@react-native-firebase/messaging';
+
 
 const SplashScreen = ({navigation}: RootStackScreenProps<'SplashScreen'>) => {
-  const handleRefresh = () => {
-    console.log(user)
-    if (!user.loading) {
-      if (user.data && !user.error) {
-        console.log('토큰 리프레시 성공. ')
-        navigation.navigate('MainBottomTabNavigation', {
-          screen: 'Home',
-        })
-      }
-      if (!user.data && user.error) {
-        console.log('토큰 리프레시 실패. 로그인으로 이동.')
-        navigation.navigate('OnboardingNavigation', {screen: 'Login'})
-      }
-      Splash.hide()
+    const handleRefresh = () => {
+        console.log(user);
+        if (!user.loading) {
+            if (user.data && !user.error) {
+                console.log('토큰 리프레시 성공. ');
+                navigation.navigate('MainBottomTabNavigation', {
+                    screen: 'Home',
+                });
+            }
+            if (!user.data && user.error) {
+                console.log('토큰 리프레시 실패. 로그인으로 이동.');
+                navigation.navigate('OnboardingNavigation', {screen: 'Login'});
+            }
+            Splash.hide();
+        }
+    };
+
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(state => state.loginReducer.user);
+
+    async function requestUserPermission() {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        // Android 13 이상부터 수동으로 권한 요청 필요.
+        if (Platform.OS === 'android' && Platform.Version >= 33)
+            PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+        if (enabled) {
+            console.log('Authorization status:', authStatus);
+        }
     }
-  }
 
-  const dispatch = useAppDispatch()
-  const user = useAppSelector(state => state.loginReducer.user)
+    useEffect(() => {
+        dispatch(getUser());
+        requestUserPermission();
+    }, []);
 
-  useEffect(() => {
-    dispatch(getUser())
-  }, [])
+    useEffect(() => {
+        // Get the device token
+        messaging()
+            .getToken()
+            .then(token => {
+                console.log(token)
+            });
 
-  useEffect(handleRefresh, [user])
-  return <View style={[{flex: 1, backgroundColor: ''}]}></View>
-}
-export default SplashScreen
+        // If using other push notification providers (ie Amazon SNS, etc)
+        // you may need to get the APNs token instead for iOS:
+        // if(Platform.OS == 'ios') { messaging().getAPNSToken().then(token => { return saveTokenToDatabase(token); }); }
+
+        // Listen to whether the token changes
+        return messaging().onTokenRefresh(token => {
+            // Todo: save token to server
+            console.log(token)
+        });
+    }, []);
+
+    useEffect(() => {
+        console.log("message receiver registration")
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            console.log(remoteMessage)
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+    }, [])
+
+    useEffect(handleRefresh, [user]);
+    return <View style={[{flex: 1, backgroundColor: ''}]}></View>;
+};
+export default SplashScreen;
