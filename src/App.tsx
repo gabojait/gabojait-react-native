e6699cef7c275e406155bf8f7094e4ec4dd5ceb9
이용인 <yongin0517@gmail.com>
2023-08-10 22:55:10 +0900

    IGNORE
    removed import
    아무것도 x

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,66 @@
-import {AppRegistry, SafeAreaView} from 'react-native';
-import {ThemeProvider} from '@rneui/themed';
-import {RootNavigation} from './presentation/navigation/RootNavigation';
+import { AppRegistry, SafeAreaView } from 'react-native';
+import { ThemeProvider } from '@rneui/themed';
+import { RootNavigation } from './presentation/navigation/RootNavigation';
 import allReducers from '@/redux/reducers';
 import ReduxThunk from 'redux-thunk';
-import {ModalProvider} from './presentation/components/modal/context';
-import {theme} from './presentation/theme';
-import ErrorBoundary from './presentation/components/errorComponent/ErrorBoundary';
-import {Fallback500, Fallback503} from './presentation/components/errorComponent/GeneralFallback';
-import React, {useEffect} from 'react';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {createLogger} from 'redux-logger';
+import { ModalProvider } from './presentation/components/modal/context';
+import { theme } from './presentation/theme';
+import GeneralErrorBoundary from './presentation/components/errorComponent/ErrorBoundary';
+import { Fallback500, Fallback503 } from './presentation/components/errorComponent/Fallback';
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createLogger } from 'redux-logger';
 import './assets/locales/index';
 
 const queryClient = new QueryClient();
 const logger = createLogger();
 const store = createStore(allReducers, applyMiddleware(ReduxThunk, logger));
 
-import NetInfo from '@react-native-community/netinfo'
-import {onlineManager} from 'react-query'
-import messaging, {firebase} from "@react-native-firebase/messaging";
+import NetInfo from '@react-native-community/netinfo';
+import { onlineManager } from 'react-query';
+import messaging, { firebase } from '@react-native-firebase/messaging';
 
 onlineManager.setEventListener(setOnline => {
-    return NetInfo.addEventListener(state => {
-        setOnline(!!state.isConnected)
-    })
-})
+  return NetInfo.addEventListener(state => {
+    setOnline(!!state.isConnected);
+  });
+});
 
 // Check if app was launched in the background and conditionally render null if so
-export function HeadlessCheck({isHeadless}: { isHeadless: boolean }) {
-    if (isHeadless) {
-        // App has been launched in the background by iOS, ignore
-        return null;
-    }
+export function HeadlessCheck({ isHeadless }: { isHeadless: boolean }) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
 
-    // Render the app component on foreground launch
-    return <App/>;
+  // Render the app component on foreground launch
+  return <App />;
 }
 
 const App = () => {
-    const backgroundStyle = {
-        flex: 1,
-    };
+  const backgroundStyle = {
+    flex: 1,
+  };
 
-    // const modalRef = useRef<CustomModalRef>()
+  // const modalRef = useRef<CustomModalRef>()
 
-    return (
-        <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <ErrorBoundary fallback500={Fallback500()} fallback503={Fallback503()}>
-                    <QueryClientProvider client={queryClient}>
-                        <ModalProvider>
-                            <SafeAreaView style={backgroundStyle}>
-                                <RootNavigation/>
-                            </SafeAreaView>
-                        </ModalProvider>
-                    </QueryClientProvider>
-                </ErrorBoundary>
-            </ThemeProvider>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GeneralErrorBoundary fallback500={Fallback500()} fallback503={Fallback503()}>
+          <QueryClientProvider client={queryClient}>
+            <ModalProvider>
+              <SafeAreaView style={backgroundStyle}>
+                <RootNavigation />
+              </SafeAreaView>
+            </ModalProvider>
+          </QueryClientProvider>
+        </GeneralErrorBoundary>
+      </ThemeProvider>
+    </Provider>
+  );
 };
-
 
 export default App;
