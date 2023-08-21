083772bd09a3f9dca7f4b5a2bcd971c33b79ef48
이용인 <yongin0517@gmail.com>
2023-08-21 16:00:51 +0900

    CHORE:Add eslint property

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugin: ['react', '@typesript-eslint'],
   rules: {
+    'no-unused-vars': 'off',
     'react-native/no-inline-styles': 1,
   },
 };
