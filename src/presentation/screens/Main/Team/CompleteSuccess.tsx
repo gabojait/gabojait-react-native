a36e6da080764d5ee99fb96e99b5cd55664de775
이용인 <yongin0517@gmail.com>
2023-08-21 15:51:40 +0900

    CHORE: Add eslint property

--- src/presentation/screens/Main/Team/CompleteSuccess.tsx
+++ src/presentation/screens/Main/Team/CompleteSuccess.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import { FilledButton } from '@/presentation/components/Button';
 import { useTheme } from '@rneui/themed';
 import { Text, View } from 'react-native';
